20c1938b45f8e4b8e6e22c374022d3fcaaf3a362
Brandon Goodman <bran99@gmail.com>
2015-07-16 15:53:41 -0400

    Removed email field from user table

--- db/migrate/20150715141124_create_users.rb
+++ db/migrate/20150715141124_create_users.rb
@@ -1,9 +1,8 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :name, null: false
+      t.string :name, null: false, uniqueness: true
       t.string :password_digest, null: false
-      t.string :email
       t.string :gotchi_name, null: false
       t.datetime :last_login, null: false, default: Time.now
 
