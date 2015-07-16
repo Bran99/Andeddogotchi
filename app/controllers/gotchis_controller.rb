c59e34bf222d5f8d4d4b499aeb9cd9c6f71aee61
Brandon Goodman <bran99@gmail.com>
2015-07-16 19:15:31 -0400

    Fixing gotchi bug.com

--- app/controllers/gotchis_controller.rb
+++ app/controllers/gotchis_controller.rb
@@ -1,8 +1,10 @@
 class GotchisController < ApplicationController
   def new
     @user = User.find_by(id: session[:current_user_id])
-    @user.gotchi ||= Gotchi.new
-    @user.gotchi.save
+    unless @user.gotchi
+      @user.gotchi = Gotchi.new
+      @user.gotchi.save
+    end
     session[:current_gotchi_age] = @user.gotchi.age
     redirect_to root_path
   end
