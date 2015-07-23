22da4c9b3291cabd01b944885b44b5ca797836db
Aarati Akkapeddi <aarati.akkapeddi@gmail.com>
2015-07-23 16:05:54 -0400

    working on sounds

--- app/assets/javascripts/nav.js
+++ app/assets/javascripts/nav.js
@@ -1,4 +1,40 @@
 
+////////////////////////////////
+//ANDEDDOGOTCHI GROUP PROJECT///
+////////////////////////////////
+// AUTHORS: Aarati Akkapeddi ///
+//          Brandon Goodman ////
+// PM:      Reshum Panchal /////
+// UX:      Rosemary      //////
+////////////////////////////////
+
+
+/*~~~~~~~~FUN AUDIO STUFF~~~~~~~~~~*/
+var audioElement;
+      $(document).ready(function() {
+        loadingSound = document.createElement('audio');
+        loadingSound.setAttribute('src', '/assets/loadddding.wav'); //filler audio for now
+        //audioElement.load()
+        //$.get();
+      });
+
+ var audioElement2;
+      $(document).ready(function() {
+        foodSound = document.createElement('audio');
+        foodSound.setAttribute('src', '/assets/eating.wav'); //filler audio for now
+        //audioElement.load()
+        //$.get();
+      });
+
+ var audioElement3;
+      $(document).ready(function() {
+        showerSound = document.createElement('audio');
+        showerSound.setAttribute('src', '/assets/showering.wav'); //filler audio for now
+        //audioElement.load()
+        //$.get();
+      });
+
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 /*<><><><><><><><>><><><><><><><><><><><><><><><><><><>*/
 /*<><><><><><><><>><><><><><><><><><><><><><><><><><><>*/
 /*<><><><><>REST LOGIC & BLOOD BATH LOGIC<><><><><><><>*/
