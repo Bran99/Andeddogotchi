6f78319d296f52e7dd7d575abd78148ed5babe7b
Brandon Goodman <bran99@gmail.com>
2016-05-04 12:22:21 -0400

    set :pty, true

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ set :application, 'Andeddogotchi'
 set :repo_url, 'git@github.com:Bran99/Andeddogotchi.git' # Edit this to match your repository
 set :branch, :master
 set :deploy_to, '/home/deploy/andeddogotchi'
-set :pty, false
+set :pty, true
 set :linked_files, %w{config/database.yml config/application.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
 set :keep_releases, 5
