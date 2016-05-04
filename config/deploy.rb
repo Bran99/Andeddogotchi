6b97af7c44a24190120cfce3c7cdccd40a5ea35e
Brandon Goodman <bran99@gmail.com>
2016-05-04 17:05:07 -0400

    trying something else

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ set :linked_files, %w{config/database.yml config/application.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
 set :keep_releases, 5
 set :rvm_type, :user
-ssh_options[:keys] = ["#{ENV['HOME']}/.ssh/id_rsa}"]
+set :ssh_options, {:keys => "#{ENV['HOME']}/.ssh/id_rsa"}
 
 set :puma_rackup, -> { File.join(current_path, 'config.ru') }
 set :puma_state, "#{shared_path}/tmp/pids/puma.state"
